49b69b9fda3f8e4bc3537beba78a5794090e217d
Simon Danisch <sdanisch@gmail.com>
2015-02-09 23:07:23 +0100

    returned to pixelcamera

--- src/line.frag
+++ src/line.frag
@@ -1,3 +1,4 @@
+
 {{GLSL_VERSION}}
 
 
@@ -9,15 +10,12 @@
 
 uniform float linewidth;
 
-float aastep(float threshold, float dist) {
-	float afwidth = 0.9 * length(vec2(dFdx(dist), dFdy(dist)));
-	return smoothstep(threshold - afwidth, threshold + afwidth, dist);
-}
 
 void main() {
 
-	float dist = length(middle - V) / (linewidth/2);
-	float alpha = 1-aastep(0.6, dist);
-	fragment_color = vec4(0,0,0, alpha);
+	float dist  = length(middle - V) / (linewidth/2);
+	float width = fwidth(dist);
+	float alpha = 1-smoothstep(0.9, 1.0, dist);
+	fragment_color = vec4(0, 0, 0,alpha);
 	fragment_objectid = uvec2(0); // not needed yet, but later for point selection and editing
 }
--- src/line.jl
+++ src/line.jl
@@ -1,14 +1,12 @@
 using GLAbstraction, ModernGL, GLWindow, GLFW
 
 
-window 	= createwindow("GLPlot", 1500,1000, windowhints=[(GLFW.SAMPLES, 0)]) 
+window 	= createwindow("GLPlot", 1920,1280, windowhints=[(GLFW.SAMPLES, 0)]) 
 ocamera = OrthographicPixelCamera(window.inputs)
 
-view = [
-  "GLSL_EXTENSIONS"     => "#extension GL_ARB_draw_instanced : enable",
-]
 
-points = Vec3[Vec3(sin(i), i, 0) * 100f0 for i=0:0.5:2pi]
+
+points = Vec3[Vec3(sin(i), i, 0) * 100f0 for i=0:0.1:2pi]
 
 data = [
 :vertex         	=> GLBuffer(Float32[1:4], 1), # simple quad
@@ -23,20 +21,20 @@ data = [
 
 program = TemplateProgram(
 	Pkg.dir("GLDraw","src","line.vert"), Pkg.dir("GLDraw","src","line.frag"), 
-	view=view, attributes=data, fragdatalocation=[(0, "fragment_color"),(1, "fragment_objectid")]
+	attributes=data, fragdatalocation=[(0, "fragment_color"),(1, "fragment_objectid")]
 )
-obj = instancedobject(data, program, length(points))
+obj = instancedobject(data, length(points)-3, program)
 prerender!(obj, enabletransparency)
 
 glClearColor(1, 1, 1, 1.0)
 
-while !GLFW.WindowShouldClose(window.glfwWindow)
+while !GLFW.WindowShouldClose(window.nativewindow)
     yield()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    
+    glViewport(0,0,window.inputs[:framebuffer_size].value...)
     render(obj)
     
-    GLFW.SwapBuffers(window.glfwWindow)
+    GLFW.SwapBuffers(window.nativewindow)
 	GLFW.PollEvents()
 end
 GLFW.Terminate()
